112ebc24bd230ef84c125a89824561b510fb6f2e
kdong <khoidong.ca@gmail.com>
2023-11-25 15:29:27 -0600

    add Login and Signup link to navigation bar

--- nav.php
+++ nav.php
@@ -28,6 +28,9 @@
         <li class="nav-item">
           <a class="nav-link" href="logout.php">Logout</a>    
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="signup.php">Sign Up</a>    
+        </li>
       </ul>
       <form  action="index.php" method="get">
             <div class="input-group" style="max-width:20rem;">
