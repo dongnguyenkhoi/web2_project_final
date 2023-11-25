05d24dae490feece0cc8339b3ab2893abff627e9
kdong <khoidong.ca@gmail.com>
2023-11-25 15:28:15 -0600

    modify signup.php

--- signup.php
+++ signup.php
@@ -4,19 +4,16 @@ require('connect.php');
 session_start();
 $username = $username_err = "";
 $password = $password_err = "";
-$captcha_err = "";
-$confirm_password = $confirm_password_err = "";
-
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     // Validate username
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
+        $username_err = "* Please enter a username.";
     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
         $username_err = "Username can only contain letters, numbers, and underscores.";
     } else{
         // Prepare a select statement
-        $query = "SELECT user_id FROM users WHERE user_name = :username";
+        $query = "SELECT user_id FROM user WHERE user_name = :username";
         
         if($statement = $db->prepare($query)){
             // Set parameters
@@ -47,21 +44,13 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         $password = trim($_POST["password"]);
     }
     
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = " * Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = " * Password did not match.";
-        }
-    }
+    
    
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err)){
         
         // Prepare an insert statement
-        $query = "INSERT INTO users (user_name, password) VALUES (:username, :password)";
+        $query = "INSERT INTO user (user_name, password) VALUES (:username, :password)";
          
         if($statement = $db->prepare($query)){
             // Set parameters
@@ -89,21 +78,24 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <div class="container">
     <?php require("nav.php") ?>
     <div class="card p-3" style="max-width: 400px;">
-    <h3>Login</h3>
-    <form action="login.php" method="post">
+    <h5>Use username or email for sign up</h5>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
         <div class="mb-3 row">
             <label for="inputUsername" class="col-sm-4 col-form-label">Username or Email</label>
             <div class="col-sm-8">
-                <input type="text" name="username" class="form-control" id="inputUsername">
+                <input type="text" name="username" class="form-control <?= (!empty($username_err)) ? 'is-invalid' : ''; ?>" id="inputUsername">
+                <span class=""><?= $username_err; ?></span>
             </div>
             <label for="inputPassword" class="col-sm-4 col-form-label">Password</label>
             <div class="col-sm-8">
-                <input type="password" name="password" class="form-control" id="inputPassword">
+                <input type="password" name="password" class="form-control <?= (!empty($password_err)) ? 'is-invalid' : ''; ?>" id="inputPassword" >
+                <span class=""><?= $password_err; ?></span>
             </div>
             
             <button class="col-4 ms-auto mt-2 btn btn-primary" type="submit">Sign Up</button>
         </div>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
     </form>
     </div>
     <?php require("footer.php") ?>
